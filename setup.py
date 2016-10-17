1c849c186b39552cff6e8a8d9794212c819b0b53
Rene Sorel <rene@proversity.org>
2016-10-17 14:18:43 +0200

    set package name to edx-xblock-bibblio

--- setup.py
+++ setup.py
@@ -21,10 +21,10 @@ def package_data(pkg, roots):
 
 
 setup(
-    name='bibblio-xblock',
+    name='edx-xblock-bibblio',
     version='0.1',
-    description='bibblio XBlock',   # TODO: write a better description.
-    license='UNKNOWN',          # TODO: choose a license: 'AGPL v3' and 'Apache 2.0' are popular.
+    description='XBlock for Bibblio Recommendations',
+    license='AGPL v3',
     packages=[
         'bibblio',
     ],
