a2c579e4f4b3a71b106ea44c063a0d4917c66c0f
renevatium <rene@proversity.org>
2017-04-18 10:44:44 +0200

    load bib-related-content before bibblio js

--- bibblio/bibblio.py
+++ bibblio/bibblio.py
@@ -62,9 +62,9 @@ class BibblioXBlock(XBlock, StudioEditableXBlockMixin, XBlockWithSettingsMixin):
         frag.add_css(self.resource_string("static/css/bibblio.css"))
         frag.add_css(self.resource_string("static/css/bib-related-content.css"))
         frag.add_javascript_url(fontawesome_js)
-        frag.add_javascript(self.resource_string("static/js/bibblio.js"))
-        frag.add_javascript(self.resource_string("static/js/underscore-min.js"))
+        frag.add_javascript(self.resource_string("static/js/underscore-min.js"))     
         frag.add_javascript(self.resource_string("static/js/bib-related-content.js"))
+        frag.add_javascript(self.resource_string("static/js/bibblio.js"))    
         frag.initialize_js('BibblioXBlock')
 
         return frag
